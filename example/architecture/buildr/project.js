a9d25ff57853090191b78bb0f7200ca07d0204a9
Andreas Siebert <andrej.siebert@gmail.com>
2013-02-16 16:39:07 +0100

    added buildrResult to handle logs and warnings

--- example/architecture/buildr/project.js
+++ example/architecture/buildr/project.js
@@ -1,4 +1,4 @@
-BUILDR_PATH = "C:/home/projects/android-mrtan/sources/sdk-apklib/app-buildr/script";
+BUILDR_PATH = "C:/home/projects/android-mrtan/sources/sdk-apklib/app-buildr/script/lib";
 
 var Buildr = require( BUILDR_PATH + "/Buildr.js" );
 console.log( "-- buildr loaded: " + Buildr );
@@ -7,6 +7,11 @@ var buildr = new Buildr(BUILDR_PATH + "/example/gen",
 		BUILDR_PATH + "/example" );
 
 buildr.deleteProject();
+
+//kopiert aus dem source-folder die betroffene datei!
+//erstellt eine Template-Datei!
+//
+//buildr.createTemplate();
 
 buildr.use( "create-helloworld-java" );
 
