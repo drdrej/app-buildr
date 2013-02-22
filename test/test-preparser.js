fffcb7021e3085f4957dd0c21ac342c3cac72edb
Andreas Siebert <andrej.siebert@gmail.com>
2013-02-22 18:41:28 +0100

    parser preprocessor modifs

--- test/test-preparser.js
+++ test/test-preparser.js
@@ -1,48 +1,55 @@
 var assert = require("assert");
 
-//var parser = require( "../lib/BuildrMacroParser.js" );
 var MacroParser = require( "../lib/MacroParser.js" );
-//var grammar = MacroParser.grammar;
+var mp = new MacroParser( "", "" );
 
-//var Parser = require("jison").Parser;
-
-describe('BuildrMacroParser', function(){
+describe( 'MacroParser', function(){
   describe('#parse()', function(){
     
 	it('replace command with one option AND word-syntax', function() {
-		var mp = new MacroParser( "", "" );
-		
-		// -- single output commands :::
-		// test: replace next word with mustache-command
-		var commandStr = "[#replace with:xyz ]";
-		var result = mp.parse( commandStr );
+		var commandStr = "[#replace with:xyz #]";
+		var result = mp.parseCommand( commandStr );
 		
 		assert.equal( "replace", result.name );
 		assert.equal( "xyz", result.params[ "with" ] );
-		
-//		{
-//			   "name": "rw",
-//			   "params": {
-//			      "dd": "zz",
-//			      "xxx": "andrej",
-//			      "aaa": "xxx"
-//			   }
-//		};
     });
 
 	it('replace command with one option in string-syntax', function() {
-		var mp = new MacroParser( "", "" );
-		
-		// -- single output commands :::
-		// test: replace next word with mustache-command
-		var commandStr = '[#replace with:"xyz" ]';
-		var result = mp.parse( commandStr );
+		var commandStr = '[#replace with:"xyz" #]';
+		var result = mp.parseCommand( commandStr );
 		
 		assert.equal( "replace", result.name );
 		assert.equal( "xyz", result.params[ "with" ] );
+		
+    });
+
+	it('replace command with one option in mustache-syntax', function() {
+		var commandStr = '[#replace with:{{xyz}} #]';
+		var result = mp.parseCommand( commandStr );
+		
+		assert.equal( "replace", result.name );
+		assert.equal( "{{xyz}}", result.params[ "with" ] );
 		
     });
 	
   });
+
+//  var commandStr = "some text [#replace with:xyz ] word some text";
+//	var result = mp.preProcessContent( commandStr );
+	
   
+  describe('#preProcessContent()', function(){
+		it( 'found command-open-tag', function() {
+			var path = __dirname + "/output.test";
+			
+			var result = mp.preProcessContent("dasdasdas[#sadasdasdas", path);
+		
+			assert.equal( "end-position is wrong", result.msg );
+//			assert.equal( "xyz", result.params[ "with" ] );
+	    });
+
+
+	  });
+
 });
+
