68883429d4292810df7e34c7d1b7ae9b5a9c9736
Andreas Siebert <andrej.siebert@gmail.com>
2013-02-22 20:49:00 +0100

    impl initCmd() - commands will be loaded from replace/ folder

--- test/test-preparser.js
+++ test/test-preparser.js
@@ -1,3 +1,27 @@
+/*
+
+Copyright (c) 2012-2013 Andreas Siebert, ask@touchableheroes.com
+
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+IN THE SOFTWARE.
+
+*/
+
 var assert = require("assert");
 
 var MacroParser = require("../lib/MacroParser.js");
@@ -25,16 +49,15 @@ describe('MacroParser', function() {
 		it('found command-open-tag', function() {
 			var path = __dirname + "/output.test";
 
-			var result = mp.preProcessContent("dasdasdas[#sadasdasdas", path);
+			var result = mp.preProcessContent("dasdasdas[#replace", path);
 
 			assert.equal("end-position is wrong", result.msg);
-			// assert.equal( "xyz", result.params[ "with" ] );
 		});
 
 		it('found command-open-tag', function() {
 			var path = __dirname + "/output2.test";
 
-			var result = mp.preProcessContent("dasdasdas[#sadasdasdas #]asd",
+			var result = mp.preProcessContent("dasdasdas[#replace #]asd",
 					path);
 			
 			assert.equal(true, result);
@@ -73,6 +96,27 @@ describe('MacroParser', function() {
 
 	});
 
+	describe('#initCommand()', function() {
+		it('init replace-command with one parameter ("with") ', function() {
+			var cmd = mp.initCmd( "[#uncomment#]" );
+
+			var valid = cmd.validate();
+			console.log( "command  found ::: %j", cmd );
+
+			assert.equal( true, valid.success );
+			assert.equal("uncomment", cmd.name);
+		});
+		
+		it('init replace-command with one parameter ("with") ', function() {
+			var commandStr = "[#replace with:xyz #]";
+			var result = mp.parseCommand(commandStr);
+
+			assert.equal("replace", result.name);
+			assert.equal("xyz", result.params["with"]);
+			
+		});
+
+	});
 
 
 });
